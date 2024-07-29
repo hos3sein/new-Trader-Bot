d238febdaf9b41a9c7fdc9ea2586bd2fa9d5231f
hossein <kianlucifer0098@gmail.com>
2024-07-29 08:32:42 -0700

    firstEdition

---
+++ position.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+class Position:
+    def __init__(self):
+        self.positionType = None
+        self.state = 0
+        self.isActive = False
+        self.reTouch = None
+        self.divergance = None
+        self.maxTouch={'price' : None , 'rsi' :None }
+        self.minTouch={'price' : None , 'rsi' :None }
+
+
+
+    def changeStatus():
+        pass
+
+
+    
+    

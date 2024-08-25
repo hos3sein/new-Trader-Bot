6374b0eb7de933251b5449b0bc9d75c9bf4b34df
hossein <kianlucifer0098@gmail.com>
2024-08-25 17:09:50 +0330

    iaasdf

--- position.py
+++ position.py
@@ -5,11 +5,12 @@ import datetime
 import logging
 import socketio
 import pytz
-import psutil
-IP=[(k, addr.address) for k, v in psutil.net_if_addrs().items() for addr in v if addr.family == -1]
+import subprocess
+IP = subprocess.check_output('wmic bios get serialnumber').decode("utf-8") 
+# IP=[(k, addr.address) for k, v in psutil.net_if_addrs().items() for addr in v if addr.family == -1]
 print(IP)
 sio = socketio.SimpleClient()
-sio.connect('https://test.spider-cryptobot.site', namespaces='/position' ,  headers = {'MACAddress' : IP[len(IP)-1][1]})
+sio.connect('https://test.spider-cryptobot.site', namespaces='/position' ,  headers = {'MACAddress' : IP})
 
 
 def message(msg):
